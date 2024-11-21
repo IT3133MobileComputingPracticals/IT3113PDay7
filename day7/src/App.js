6094d1672213f1f4df615a5edab5da0ddca9f8a1
Lasantha Kumara <lasanthakumaraofficial@gmail.com>
2024-11-21 16:52:46 +0000

    edited App.js

--- day7/src/App.js
+++ day7/src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h2>Hello React I'm Parent</h2>
-      <Childcom setfun={receiveName}/>
+      <Child setfun={receiveName} />
       <p>Parent:My child said:{childname}</p>
     </div>
   );
